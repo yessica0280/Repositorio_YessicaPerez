a858630abdc4104f6648718a5e96c84f4d78c423
yessica perez <yp80905@gmail.com>
2025-03-16 11:49:46 -0500

    Feat :sparkles: Arragements

--- funcion.js
+++ funcion.js
@@ -4,11 +4,14 @@ function sol(){
     document.documentElement.style.setProperty("--fondo-color", color_fondo_actual);
 
     let color_fondo1= getComputedStyle(document.documentElement).getPropertyValue("--fondo-color1");
-    color_fondo1= color_fondo1 === "#9acef4"?"#29314a":"#9acef4";
+    color_fondo1= color_fondo1 === "#9acef4"?"#598aaa":"#9acef4";
     document.documentElement.style.setProperty("--fondo-color1", color_fondo1);
 
     let deg1= getComputedStyle(document.documentElement).getPropertyValue("--deg-uno");
     deg1= deg1 === "#E1BEE7"?"#29314a":"#E1BEE7";
     document.documentElement.style.setProperty("--deg-uno", deg1);
 
+    let letra= getComputedStyle(document.documentElement).getPropertyValue("--letra-uno");
+    letra= letra === "#3F51B5"?"black":"#3F51B5";
+    document.documentElement.style.setProperty("--letra-uno", letra);
 }
