4a9847b44dc47787f5a1f684e9770ab556052edf
yessica0280 <165828666+yessica0280@users.noreply.github.com>
2025-03-17 09:55:28 -0500

    Feat :sparkles: Arragements

---
+++ Repositorio_YessicaPerez/funcion.js
@@ -0,0 +1,19 @@
+function sol(){
+    let color_fondo_actual = getComputedStyle(document.documentElement).getPropertyValue("--fondo-color");
+    color_fondo_actual = color_fondo_actual === "white"?"black":"white";
+    document.documentElement.style.setProperty("--fondo-color", color_fondo_actual);
+
+    let color_fondo1= getComputedStyle(document.documentElement).getPropertyValue("--fondo-color1");
+    color_fondo1= color_fondo1 === "#9acef4"?"#598aaa":"#9acef4";
+    document.documentElement.style.setProperty("--fondo-color1", color_fondo1);
+
+    let deg1= getComputedStyle(document.documentElement).getPropertyValue("--deg-uno");
+    deg1= deg1 === "#E1BEE7"?"#48566a":"#E1BEE7";
+    document.documentElement.style.setProperty("--deg-uno", deg1);
+
+    let letra= getComputedStyle(document.documentElement).getPropertyValue("--letra-uno");
+    letra= letra === "#3F51B5"?"white":"#3F51B5";
+    document.documentElement.style.setProperty("--letra-uno", letra);
+
+    
+}
