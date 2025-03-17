06bab8b88da05d17940341319bcfd14b34d0798c
yessica0280 <yp80905@gmail.com>
2025-03-17 09:57:31 -0500

    Feat :sparkles: Arragements

--- funcion.js
+++ funcion.js
@@ -8,10 +8,12 @@ function sol(){
     document.documentElement.style.setProperty("--fondo-color1", color_fondo1);
 
     let deg1= getComputedStyle(document.documentElement).getPropertyValue("--deg-uno");
-    deg1= deg1 === "#E1BEE7"?"#29314a":"#E1BEE7";
+    deg1= deg1 === "#E1BEE7"?"#48566a":"#E1BEE7";
     document.documentElement.style.setProperty("--deg-uno", deg1);
 
     let letra= getComputedStyle(document.documentElement).getPropertyValue("--letra-uno");
-    letra= letra === "#3F51B5"?"black":"#3F51B5";
+    letra= letra === "#3F51B5"?"white":"#3F51B5";
     document.documentElement.style.setProperty("--letra-uno", letra);
+
+    
 }
