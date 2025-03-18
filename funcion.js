6c41670784d2ce9186610c4ff02dde14fc9ce7b3
yessica perez <yp80905@gmail.com>
2025-03-17 23:34:52 -0500

    Feat :sparkles: Arragements

--- funcion.js
+++ funcion.js
@@ -1,6 +1,6 @@
 function sol(){
     let color_fondo_actual = getComputedStyle(document.documentElement).getPropertyValue("--fondo-color");
-    color_fondo_actual = color_fondo_actual === "white"?"black":"white";
+    color_fondo_actual = color_fondo_actual === "black"?"#0e4089":"black";
     document.documentElement.style.setProperty("--fondo-color", color_fondo_actual);
 
     let color_fondo1= getComputedStyle(document.documentElement).getPropertyValue("--fondo-color1");
@@ -8,12 +8,19 @@ function sol(){
     document.documentElement.style.setProperty("--fondo-color1", color_fondo1);
 
     let deg1= getComputedStyle(document.documentElement).getPropertyValue("--deg-uno");
-    deg1= deg1 === "#E1BEE7"?"#48566a":"#E1BEE7";
+    deg1= deg1 === "#E1BEE7"?"#586982":"#E1BEE7";
     document.documentElement.style.setProperty("--deg-uno", deg1);
 
     let letra= getComputedStyle(document.documentElement).getPropertyValue("--letra-uno");
-    letra= letra === "#3F51B5"?"white":"#3F51B5";
+    letra= letra === "#3F51B5"?"#00255e":"#3F51B5";
     document.documentElement.style.setProperty("--letra-uno", letra);
 
-    
+    let letra1= getComputedStyle(document.documentElement).getPropertyValue("--letra-unoo");
+    letra1= letra1 === "#2789d4"?"#1b3969":"#2789d4";
+    document.documentElement.style.setProperty("--letra-unoo", letra1);
+
+    let letra2= getComputedStyle(document.documentElement).getPropertyValue("--letra-dos");
+    letra2= letra2 === "#3F51B5"?"black":"#3F51B5";
+    document.documentElement.style.setProperty("--letra-dos", letra2);
 }
+
